157708bd2179e1a3c4000d186c01051a619c99ad
alexandrosilvaagile <alexandro.silva@outlook.com>
2022-10-07 15:25:38 -0300

    meu primiero commit

---
+++ MoneyExtension.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MoneyExtension.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ MoneyExtension/Money.cs
@@ -0,0 +1,14 @@
+﻿namespace MoneyExtension;
+public static class Money
+{
+    public static class MoneyExtensions
+    {
+        public static int ToCents(decimal amount)
+        {
+            if(amount < 0)
+                return 0;
+
+            return Decimal.ToInt32(amount);
+        }
+    }
+}
